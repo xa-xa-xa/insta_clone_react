468d674ac22d3e8b9b307afeef250c604d7a7cdd
xa-xa-xa <askrasn@gmail.com>
2024-02-08 16:45:05 -0800

    updateProfile

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -21,9 +21,28 @@ export const SigninValidation = z.object({
     .min(8, { message: "Password must be at least 8 characters." }),
 });
 
-export const PostValidation = z.object({
-  caption: z.string().min(5).max(2200),
+export const ProfileValidation = z.object({
   file: z.custom<File[]>(),
-  location: z.string().min(2).max(1000),
+  name: z.string().min(2, { message: "Name must be at least 2 characters." }),
+  username: z
+    .string()
+    .min(2, { message: "Name must be at least 2 characters." }),
+  email: z.string().email(),
+  bio: z.string(),
+});
+
+// ============================================================
+// POST
+// ============================================================
+export const PostValidation = z.object({
+  caption: z
+    .string()
+    .min(5, { message: "Minimum 5 characters." })
+    .max(2200, { message: "Maximum 2,200 caracters" }),
+  file: z.custom<File[]>(),
+  location: z
+    .string()
+    .min(1, { message: "This field is required" })
+    .max(1000, { message: "Maximum 1000 characters." }),
   tags: z.string(),
 });
